71612ee5bdb5b61c1a5daf690d818504db79a002
iamchristopher <chriswright@live.com>
2015-08-02 20:45:34 -0400

    Changed inline event handlers now use prototype methods

--- app/server/lib/chat.js
+++ app/server/lib/chat.js
@@ -9,34 +9,39 @@ function Chat (socket) {
 
     this._socket = socket;
 
-    this._socket.on('chat.channel.join', function (payload) {
-        var room = Room.join(payload.room, this.id, {
-            name: payload.sender
-        });
-        this.join(room);
-        this.to(room).emit('chat.message.receive', {
-            message: payload.sender + ' has joined.'
-        });
-    });
-
-    this._socket.on('chat.message.send', function (payload) {
-        this.to(payload.room).emit('chat.message.receive', {
-            message: payload.message,
-            sender: payload.sender
-        });
-    });
-
-    this._socket.on('disconnect', function () {
-        var room = Room.leave(this.id);
-
-        if (!room.id) {
-            return;
-        }
-
-        this.to(room.id).emit('chat.message.receive', {
-            message: room.client.name + ' has left.'
-        });
-    });
+    this._socket.on('chat.channel.join', this.channelJoinHandler);
+    this._socket.on('chat.message.send', this.sendMessageHandler);
+    this._socket.on('disconnect', this.disconnectHandler);
 }
+
+Chat.prototype.channelJoinHandler = function (payload) {
+    var room = Room.join(payload.room, this.id, {
+        name: payload.sender
+    });
+
+    this.join(room);
+    this.to(room).emit('chat.message.receive', {
+        message: payload.sender + ' has joined.'
+    });
+};
+
+Chat.prototype.sendMessageHandler =function (payload) {
+    this.to(payload.room).emit('chat.message.receive', {
+        message: payload.message,
+        sender: payload.sender
+    });
+};
+
+Chat.prototype.disconnectHandler = function () {
+    var room = Room.leave(this.id);
+
+    if (!room.id) {
+        return;
+    }
+
+    this.to(room.id).emit('chat.message.receive', {
+        message: room.client.name + ' has left.'
+    });
+};
 
 module.exports = Chat;
