6fd7f6301862591c7ab7cf0322c6a76dc4b324a1
Chris Wright <chris@arcane.ws>
2017-03-13 19:37:15 -0400

    Added connect/ disconnect messages

--- app/index.js
+++ app/index.js
@@ -2,6 +2,7 @@ import socketio from 'socket.io';
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+import * as roomManager from './components/room';
 
 const app = express();
 const server = http.Server(app);
@@ -11,6 +12,45 @@ app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
 }));
+
+io.on('connection', socket => {
+    let _room = null;
+
+    socket.on('CHAT_JOIN_ROOM', ({ user, room }) => {
+        _room = roomManager
+            .add({
+                id: room
+            })
+            .join({
+                user: {
+                    id: socket.id,
+                    name: user
+                }
+            });
+
+        socket.broadcast.emit('MESSAGE', {
+            sender: 'System',
+            text: `${user} has joined the fight!`
+        });
+    });
+
+    socket.on('disconnect', () => {
+        const {
+            clients: {
+                [socket.id]: user
+            }
+        } = _room;
+
+        _room.leave({
+            user: socket.id
+        });
+
+        socket.broadcast.emit('MESSAGE', {
+            sender: 'System',
+            text: `${user.name} has abandoned the fight!`
+        });
+    });
+});
 
 server.listen(8080, () => {
     console.log('listening on *:8080');
