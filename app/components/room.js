233c1c9d1e2c4a399cfa27f977a6c4cf122a3e55
Chris Wright <chris@arcane.ws>
2017-03-13 19:36:31 -0400

    Added room manager component

---
+++ app/components/room.js
@@ -0,0 +1,60 @@
+let rooms = {};
+
+export function add ({
+    id
+} = {}) {
+    const room = get({ id });
+
+    if (room !== undefined) {
+        return room;
+    }
+
+    return rooms[id] = {
+        id,
+        clients: {},
+        join: ({ user }) => join({
+            id,
+            user
+        }),
+        leave: ({ user }) => leave({
+            id,
+            user
+        })
+    };
+}
+
+export function get ({
+    id
+} = {}) {
+    return rooms[id];
+}
+
+export function join ({
+    id,
+    user
+} = {}) {
+    const room = get({ id });
+    const {
+        id: userId,
+        ...userData
+    } = user;
+
+    room.clients[userId] = userData;
+
+    return room;
+}
+
+export function leave ({
+    id,
+    user
+} = {}) {
+    const room = get({ id });
+    const {
+        [user.toString()]: deletedUser,
+        ...clients
+    } = room.clients;
+
+    room.clients = clients;
+
+    return room;
+}
