f5f3d158789c7a42c7cff7a8428f498be942f330
Chris Wright <chris@arcane.ws>
2017-03-29 20:57:06 -0400

    Changed Pawns are now correctly synced across connect/ disconnect events

--- app/components/pawn.js
+++ app/components/pawn.js
@@ -1,19 +1,67 @@
+import * as roomManager from './room';
+
 export default function ({
     socket,
     type,
     payload
 } = {}) {
-    const [
-        isolatedType,
-        ...rest
-    ] = type.split('_');
+    const {
+        handshake: {
+            query: {
+                room
+            }
+        }
+    } = socket;
+    const roomData = roomManager
+        .get({
+            id: room
+        });
+    const {
+        clients: {
+            [socket.id]: client,
+            ...clients
+        }
+    } = roomData;
 
-    switch (rest.join('_')) {
-        case 'MOVE':
-            return socket.broadcast.emit(type, payload);
-        case 'REGISTER':
-            return socket.broadcast.emit(type, payload);
+    switch (type) {
+        case 'PAWN_MOVE':
+            return socket.broadcast.to(room).emit(type, payload);
+        case 'PAWN_REGISTER':
+            let {
+                id,
+                ...pawn
+            } = payload;
+
+            client.pawns[id] = pawn;
+
+            return socket.broadcast.to(room).emit(type, {
+                ...payload,
+                id,
+                owner: socket.id
+            });
+        case 'CONNECT':
+            return Object.keys(clients)
+                .map(id => ({
+                    ...clients[id],
+                    id
+                }))
+                .forEach(({ id, pawns }) => Object.keys(pawns)
+                    .map(id => pawns[id])
+                    .forEach(pawn => socket.emit('PAWN_REGISTER', {
+                        ...pawn,
+                        id,
+                        owner: id
+                    }))
+                );
+        case 'DISCONNECT':
+            roomData.leave({
+                user: socket.id
+            });
+
+            return socket.broadcast.to(room).emit('PAWN_UNREGISTER', {
+                owner: socket.id
+            });
         default:
-            console.log(type, payload);
+            console.log('"', type, payload);
     }
 };
