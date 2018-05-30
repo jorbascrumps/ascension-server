9083ccf1633cc18ae502b936408304b1b7dc8cac
iamchristopher <chriswright@live.com>
2018-05-30 16:06:09 -0400

    Added game flow with boardgame.io

---
+++ app/boardgame.js
@@ -0,0 +1,12 @@
+import {
+    Server
+} from 'boardgame.io/server';
+import {
+    Game
+} from 'boardgame.io/core';
+import game from './game';
+
+const server = Server({
+    games: [ Game(game) ]
+});
+server.run(process.env.PORT, () => console.log(`App launched @ ::${process.env.PORT}`));
--- index.js
+++ index.js
@@ -2,4 +2,4 @@ require('babel-register')({
     ignore: filename => filename.indexOf('/app/') === -1
 });
 require('babel-polyfill');
-require('./app');
+require('./app/boardgame');
