7dd3192b1bd3a205c353592577307522a4ec97b8
iamchristopher <chriswright@live.com>
2018-07-15 18:13:22 -0400

    Changed map data is now provided by the server

--- app/boardgame.js
+++ app/boardgame.js
@@ -4,9 +4,32 @@ import {
 import {
     Game
 } from 'boardgame.io/core';
-import game from '../../common/game';
+
+import gameConfig from '../../common/game';
+import {
+    chunkMapData
+} from '../../common/util';
+import {
+    layers as mapLayers
+} from '../../common/data/maps/level';
 
 const PORT = parseInt(process.env.PORT, 10) || 8080;
+
+const {
+    data: mapData
+} = mapLayers.find(({ name }) => name === 'map');
+const {
+    data: blockedData
+} = mapLayers.find(({ name }) => name === 'blocked');
+const {
+    data: interactionsData
+} = mapLayers.find(({ name }) => name === 'interactions');
+
+const game = gameConfig({
+    map: chunkMapData(mapData, 20),
+    blocked: chunkMapData(blockedData, 20),
+    interactions: chunkMapData(interactionsData, 20)
+});
 const server = Server({
     games: [ Game(game) ]
 });
